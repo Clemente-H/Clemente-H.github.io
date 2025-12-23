468501ac0fab9aca854b322a51b32287e001d6d1
Clemente <clemente@Clementes-MacBook-Pro.local>
2025-12-23 10:18:09 -0300

    update bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import { TeachingPage } from "./pages/TeachingPage.tsx";
+import { ScrollToTop } from "./components/ScrollToTop.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/teaching" element={<TeachingPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
